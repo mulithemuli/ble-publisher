fdfa183823a86af8e969c0b056b42e34d17e9aa7
mulithemuli <philipp.lohmeyer@gmail.com>
2022-12-08 18:12:39 +0100

    configuration file available via argument

--- scan_publisher.py
+++ scan_publisher.py
@@ -41,37 +41,53 @@ class ScanDelegate(DefaultDelegate):
 
             if dev.addr in config['devices'] and 'name' in config['devices'][dev.addr]:
               print('device lookup for ' + dev.addr + ' resulted in name: ' + config['devices'][dev.addr]['name'])
+              sys.stdout.flush()
               dev_name = config['devices'][dev.addr]['name']
             else:
               print('using default device address instead of name for: ' + dev.addr)
+              sys.stdout.flush()
               dev_name = dev.addr
 
             json_data = '{{"mac": "{0}", "temperature": {1:5.2f}, "humidity": {2:3.2f}, "button": "{4}", "battery": {5:02.0f}, "uptime": {3:.0f}, "name": "{6}"}}'.\
                   format(dev.addr, data.tmp, data.hum, data.upt, 'On ' if data.btn else 'Off', data.btr, dev_name)
 
             print('collected data: ' + json_data)
+            sys.stdout.flush()
 
             if 'mqtt' in config:
               if config['devices'].get(dev.addr) is None or config['devices'][dev.addr].get('ignore') is None or not config['devices'][dev.addr].get('ignore') is True:
                 topic = config['mqtt']['base_topic'] + '/' + dev_name
                 print('publishing to ' + topic)
+                sys.stdout.flush()
                 client.publish(topic, json_data)
               else:
                 print('not publishing device ' + dev_name + ' since it is ignored')
+                sys.stdout.flush()
 
 def on_mqtt_connect(client, userdata, flags, rc):
   print('connected with result code ' + str(rc))
+  sys.stdout.flush()
+
+config_path = 'config.json'
+
+if len(sys.argv) > 1:
+  config_path = sys.argv[1]
+
+print('using config ' + config_path)
+sys.stdout.flush()
 
 try:
-  f = open('config.json')
+  f = open(config_path)
   config = json.load(f)
   f.close()
 except Exception as ex:
   print('no configuration found - just listing devices')
+  sys.stdout.flush()
   config = json.loads('{}')
 
 if not 'devices' in config:
   print('no devices defined - using default empty')
+  sys.stdout.flush()
   config['devices'] = json.loads('{}')
 
 scanDelegate = ScanDelegate()
@@ -81,6 +97,7 @@ if 'mqtt' in config:
   client = mqtt.Client()
   if 'user' in config['mqtt'] or 'password' in config['mqtt']:
     print('using user and password')
+    sys.stdout.flush()
     if 'user' in config['mqtt']:
       user = config['mqtt']['user']
     else:
@@ -98,9 +115,11 @@ if 'mqtt' in config:
     client.loop_start()
   except KeyboardInterrupt:
     print('\nInterrupted')
+    sys.stdout.flush()
     sys.exit(0)
 else:
   print('no mqtt configuration found - not publishing anything')
+  sys.stdout.flush()
 
 
 while True:
@@ -114,6 +133,7 @@ while True:
         pass
     except KeyboardInterrupt:
         print('\nInterrupted!')
+        sys.stdout.flush()
         sys.exit(0)
 
 
